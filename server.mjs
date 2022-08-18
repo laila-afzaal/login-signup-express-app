8e94ee7798a8e44e328b2e5940d1b72177cf6d5d
Laila Afzaal <lailaafzaal06@gmail.com>
2022-08-18 20:31:20 +0500

    commit

--- server.mjs
+++ server.mjs
@@ -2,9 +2,9 @@ import express from 'express'
 import cors from "cors"
 import { nanoid } from 'nanoid'
 
-
-app.use(express.json());
 const app = express();
+app.use(express.json());
+
 app.use(cors());
 
 let userBase = [];
