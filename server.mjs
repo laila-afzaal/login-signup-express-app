088c574d36c337808dc72e2c4225122e4197e7eb
Laila Afzaal <lailaafzaal06@gmail.com>
2022-08-20 19:26:08 +0500

    mongodb connect commit

--- server.mjs
+++ server.mjs
@@ -1,6 +1,39 @@
 import express from 'express'
 import cors from "cors"
 import { nanoid } from 'nanoid'
+import mongoose from 'mongoose';
+
+/////////////////////-MongoDB Connecting Code-////////////////////////////////
+//boiler plate from : https://github.com/mInzamamMalik/salesman-app/blob/master/index.ts
+let dbURI = 'mongodb+srv://abc:123@cluster0.zwm89fi.mongodb.net/socialMediaBase?retryWrites=true&w=majority';
+// let dbURI = 'mongodb://localhost/mydatabase';
+mongoose.connect(dbURI);
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function() {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function() {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function(err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function() {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function() {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
 
 const app = express();
 app.use(express.json());
@@ -83,14 +116,14 @@ for(let i = 0; i < userBase.length; i++){
         firstName: userBase[i].firstName,
         lastName: userBase[i].lastName,
         email: userBase[i].email,
-        message2: "login successful",
+        message: "login successful",
         // token: "some unique token"
       })
       return;
 
     } else {
       res.status(401).send({
-        message2: "Incorrect password"
+        message: "Incorrect password"
       })
       return;
     }
@@ -99,7 +132,7 @@ for(let i = 0; i < userBase.length; i++){
 
 if(!isFound){
   res.status(404).send({
-    message2: "user not found"
+    message: "user not found"
   })
   return;
 }
