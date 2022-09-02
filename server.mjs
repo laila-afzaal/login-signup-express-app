d03cc2277318435f0f70788ee154206b655e6892
Laila Afzaal <lailaafzaal06@gmail.com>
2022-09-02 14:08:17 +0500

    commit

--- server.mjs
+++ server.mjs
@@ -147,7 +147,7 @@ app.post(`/login`, (req, res) => {
   }
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
